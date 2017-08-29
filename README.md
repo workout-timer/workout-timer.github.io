f13d9046c60fbc74a7794ca74661fd26a21e6f5c
Aaron Stacy <aaronstacy@google.com>
2017-08-28 22:52:12 -0500

    create react app

---
+++ README.md
@@ -0,0 +1,1 @@
+# QUADS OF THE GODS
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default (props) =>
+  <div className="absolute right-0 bottom-0 left-0 p4">
+    <button type="button" className="btn btn-primary p2 col-12">
+      LETS GO
+    </button>
+  </div>;
