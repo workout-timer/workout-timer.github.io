3948962289e17ed49b3b33620e8dec7de038ad41
Aaron Stacy <aaron.r.stacy@gmail.com>
2017-08-29 23:27:59 -0500

    add logo

--- src/ui/start.js
+++ src/ui/start.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 import BottomButton from './bottom-button';
+import logo from '../row.svg';
 
 export default ({onStartWorkoutClicked}) =>
-  <BottomButton onClick={onStartWorkoutClicked}>LETS GO</BottomButton>;
+  <div>
+    <img
+      src={logo}
+      alt="Rowing timer logo"
+      style={{width: '75%', maxWidth: '200px'}}
+      className="block mt4 mx-auto"/>
+    <BottomButton onClick={onStartWorkoutClicked}>LETS GO</BottomButton>
+  </div>;
