e54dc656375f73c7b36d1353a6ac3db5dece2f34
Aaron Stacy <aaron.r.stacy@gmail.com>
2017-08-30 15:52:42 -0500

    big ass button

--- src/ui/bottom-button.js
+++ src/ui/bottom-button.js
@@ -4,7 +4,7 @@ export default ({onClick, children}) =>
   <div className="absolute right-0 bottom-0 left-0 p4">
     <button
         type="button"
-        className="btn btn-primary p2 col-12"
+        className="btn btn-primary p3 col-12"
         onClick={onClick}>
       {children}
     </button>
